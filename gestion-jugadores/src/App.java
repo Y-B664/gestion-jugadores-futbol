b944bf1c44344605383130564c3f09a5c9b30134
Yudy Benavides <231582951+Y-B664@users.noreply.github.com>
2026-07-27 21:37:20 -0500

    Test listas

---
+++ gestion-jugadores/src/App.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.TreeMap;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+                List<List<String>> test = new ArrayList<>();
+                List<String> jugador = new ArrayList<>();
+
+                jugador.add("Carlos");
+                jugador.add("Bautista");
+                jugador.add("Cartagena");
+                System.out.println(jugador.get(1));
+                test.add(jugador);
+                System.out.println(test.get(0).get(0));
+
+                
+            }
+            
+        }
+
+
